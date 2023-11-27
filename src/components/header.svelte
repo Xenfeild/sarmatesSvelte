af2bdc5ac99efd3f12c4ae2eb6db9cb368030df5
xenfeild <antonio.t350@gmail.com>
2023-11-27 13:55:34 +0100

    second_commit

---
+++ src/components/header.svelte
@@ -0,0 +1,33 @@
+<style lang="scss">
+    @import"../src/style/header.scss";
+</style>
+
+<header> 
+    <img src="../src/lib/img/logo/logo_sarmates_modified.svg" alt="logo">
+    <div id="burgerIcon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 200 200">
+          <g stroke-width="7" stroke-linecap="round">
+            <path d="M72 82.286h28.75" fill="none" stroke="#fff" />
+            <path
+              d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
+              fill="none" stroke="#fff" />
+            <path d="M72 125.143h28.75" fill="none" stroke="#fff" />
+            <path
+              d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
+              fill="none" stroke="#fff" />
+            <path d="M100.75 82.286h28.75" fill="none" stroke="#fff" />
+            <path d="M100.75 125.143h28.75" fill="none" stroke="#fff" />
+          </g>
+        </svg>
+    </div>
+    <nav>
+        <ul>
+            <li>News</li>
+            <li>Live</li>
+            <li>Biography</li>
+            <li>Discography</li>
+            <li>Galeries</li>
+            <li>Mention légales</li>
+        </ul>
+    </nav>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style lang="scss">
+    @import "../src/style/style.scss";
+</style>
+
+<script>
+    import Header from "../components/header.svelte";
+</script>
+<Header />
+<main>
+    <div class="test">
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</main>
